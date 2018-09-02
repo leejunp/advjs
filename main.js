2ad27c2070104eac7b1f133401a368877dede99f
LeeJun Park <31444831+leejunp@users.noreply.github.com>
2018-09-02 03:47:37 -0400

    basic structure

---
+++ main.js
@@ -0,0 +1,58 @@
+function intro(){
+  $('.main').hide();
+  $(':button').css("text-decoration","inherit")
+  $('#intro').show()
+}
+
+function as1(){
+  $('.main').hide()
+  $(':button').css("text-decoration","inherit")
+  $('#as1').show()
+  $('#button1').css("text-decoration","underline")
+}
+
+function as2(){
+  $('.main').hide()
+  $(':button').css("text-decoration","inherit")
+  $('#as2').show()
+  $('#button2').css("text-decoration","underline")
+}
+
+// WITH DROP DOWN
+function as3(){
+  $('.main').hide()
+  $(':button').css("text-decoration","inherit")
+  $('#as3').show()
+  $('#button3').css("text-decoration","underline")
+}
+
+$('#hasdrop1').hover(
+  function(){
+    $('#drpdown1').show()
+  },
+  function(){
+    $('#drpdown1').hide()
+  }
+)
+
+function as31(){
+  $('.main').hide()
+  $(':button').css("text-decoration","inherit")
+  $('#as31').show()
+  $('#button3').css("text-decoration","underline")
+}
+
+function as32(){
+  $('.main').hide()
+  $(':button').css("text-decoration","inherit")
+  $('#as32').show()
+  $('#button3').css("text-decoration","underline")
+}
+//END OF DROP DROP DOWN
+
+function as4(){
+  $('.main').hide()
+  $(':button').css("text-decoration","inherit")
+  $('#as4').show()
+  $('#button4').css("text-decoration","underline")
+}
