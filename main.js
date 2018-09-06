bcf7ab1b1ac6b3fcd2b5a3986cbce164a18e4e09
LeeJun Park <31444831+leejunp@users.noreply.github.com>
2018-09-06 17:00:21 -0400

    responsive

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 function intro(){
   $('.main').hide();
-  $(':button').css("text-decoration","inherit")
+  $(':button').css("color","black")
   $('#intro').show()
 }
 
 function as1(){
   $('.main').hide()
-  $(':button').css("text-decoration","inherit")
+  $(':button').css("color","black")
   $('#as1').show()
-  $('#button1').css("text-decoration","underline")
+  $('#button1').css("color","#FF5400")
 }
 
 function as2(){
   $('.main').hide()
-  $(':button').css("text-decoration","inherit")
+  $(':button').css("color","black")
   $('#as2').show()
-  $('#button2').css("text-decoration","underline")
+  $('#button2').css("color","#FF5400")
 }
 
 // WITH DROP DOWN
 function as3(){
   $('.main').hide()
-  $(':button').css("text-decoration","inherit")
+  $(':button').css("color","black")
   $('#as3').show()
-  $('#button3').css("text-decoration","underline")
+  $('#button3').css("color","#FF5400")
 }
 
 $('#hasdrop1').hover(
@@ -37,22 +37,22 @@ $('#hasdrop1').hover(
 
 function as31(){
   $('.main').hide()
-  $(':button').css("text-decoration","inherit")
+  $(':button').css("color","black")
   $('#as31').show()
-  $('#button3').css("text-decoration","underline")
+  $('#button3').css("color","#FF5400")
 }
 
 function as32(){
   $('.main').hide()
-  $(':button').css("text-decoration","inherit")
+  $(':button').css("color","black")
   $('#as32').show()
-  $('#button3').css("text-decoration","underline")
+  $('#button3').css("color","#FF5400")
 }
 //END OF DROP DROP DOWN
 
 function as4(){
   $('.main').hide()
-  $(':button').css("text-decoration","inherit")
+  $(':button').css("color","black")
   $('#as4').show()
-  $('#button4').css("text-decoration","underline")
+  $('#button4').css("color","#FF5400")
 }
