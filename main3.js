6a3bb3798eb71133ed95fb5960f01f6479ce2b5e
LeeJun Park <31444831+leejunp@users.noreply.github.com>
2018-09-27 17:35:44 -0400

    j

---
+++ main3.js
@@ -0,0 +1,344 @@
+var timer = 0;
+
+for(var x = 0; x < 10; x++){
+
+  setTimeout(function(){
+
+    var hex = '0123456789ABCDEF';
+    var color = '#';
+
+    for (var z = 0; z < 6; z++) {
+      color += hex[Math.floor(Math.random() * 16)];
+    }
+
+    var circle = document.createElement("div");
+    circle.className="circle";
+    circle.style.backgroundColor = color;
+
+
+    document.getElementById("shapes").appendChild(circle);
+  }, timer)
+
+  timer+=100;
+}
+
+
+
+for(var x = 0; x < 10; x++){
+
+  setTimeout(function(){
+
+    var hex = '0123456789ABCDEF';
+    var color = '#';
+
+    for (var z = 0; z < 6; z++) {
+      color += hex[Math.floor(Math.random() * 16)];
+    }
+
+    var circle = document.createElement("div");
+    circle.className="circle";
+    circle.style.backgroundColor = color;
+
+
+    document.getElementById("shapes2").appendChild(circle);
+  }, timer)
+
+  timer+=100;
+}
+
+
+
+for(var x = 0; x < 10; x++){
+
+  setTimeout(function(){
+
+    var hex = '0123456789ABCDEF';
+    var color = '#';
+
+    for (var z = 0; z < 6; z++) {
+      color += hex[Math.floor(Math.random() * 16)];
+    }
+
+    var circle = document.createElement("div");
+    circle.className="circle";
+    circle.style.backgroundColor = color;
+
+
+    document.getElementById("shapes3").appendChild(circle);
+  }, timer)
+
+  timer+=100;
+}
+
+
+
+for(var x = 0; x < 10; x++){
+
+  setTimeout(function(){
+
+    var hex = '0123456789ABCDEF';
+    var color = '#';
+
+    for (var z = 0; z < 6; z++) {
+      color += hex[Math.floor(Math.random() * 16)];
+    }
+
+    var circle = document.createElement("div");
+    circle.className="circle";
+    circle.style.backgroundColor = color;
+
+
+    document.getElementById("shapes4").appendChild(circle);
+  }, timer)
+
+  timer+=100;
+}
+
+
+
+for(var x = 0; x < 10; x++){
+
+  setTimeout(function(){
+
+    var hex = '0123456789ABCDEF';
+    var color = '#';
+
+    for (var z = 0; z < 6; z++) {
+      color += hex[Math.floor(Math.random() * 16)];
+    }
+
+    var circle = document.createElement("div");
+    circle.className="circle";
+    circle.style.backgroundColor = color;
+
+
+    document.getElementById("shapes5").appendChild(circle);
+  }, timer)
+
+  timer+=100;
+}
+
+
+for(var x = 0; x < 10; x++){
+
+  setTimeout(function(){
+
+    var hex = '0123456789ABCDEF';
+    var color = '#';
+
+    for (var z = 0; z < 6; z++) {
+      color += hex[Math.floor(Math.random() * 16)];
+    }
+
+    var circle = document.createElement("div");
+    circle.className="circle";
+    circle.style.backgroundColor = color;
+
+
+    document.getElementById("shapes6").appendChild(circle);
+  }, timer)
+
+  timer+=100;
+}
+
+
+for(var x = 0; x < 10; x++){
+
+  setTimeout(function(){
+
+    var hex = '0123456789ABCDEF';
+    var color = '#';
+
+    for (var z = 0; z < 6; z++) {
+      color += hex[Math.floor(Math.random() * 16)];
+    }
+
+    var circle = document.createElement("div");
+    circle.className="circle";
+    circle.style.backgroundColor = color;
+
+
+    document.getElementById("shapes7").appendChild(circle);
+  }, timer)
+
+  timer+=100;
+}
+
+
+for(var x = 0; x < 10; x++){
+
+  setTimeout(function(){
+
+    var hex = '0123456789ABCDEF';
+    var color = '#';
+
+    for (var z = 0; z < 6; z++) {
+      color += hex[Math.floor(Math.random() * 16)];
+    }
+
+    var circle = document.createElement("div");
+    circle.className="circle";
+    circle.style.backgroundColor = color;
+
+
+    document.getElementById("shapes8").appendChild(circle);
+  }, timer)
+
+  timer+=100;
+}
+
+
+for(var x = 0; x < 10; x++){
+
+  setTimeout(function(){
+
+    var hex = '0123456789ABCDEF';
+    var color = '#';
+
+    for (var z = 0; z < 6; z++) {
+      color += hex[Math.floor(Math.random() * 16)];
+    }
+
+    var circle = document.createElement("div");
+    circle.className="circle";
+    circle.style.backgroundColor = color;
+
+
+    document.getElementById("shapes9").appendChild(circle);
+  }, timer)
+
+  timer+=100;
+}
+
+
+for(var x = 0; x < 10; x++){
+
+  setTimeout(function(){
+
+    var hex = '0123456789ABCDEF';
+    var color = '#';
+
+    for (var z = 0; z < 6; z++) {
+      color += hex[Math.floor(Math.random() * 16)];
+    }
+
+    var circle = document.createElement("div");
+    circle.className="circle";
+    circle.style.backgroundColor = color;
+
+
+    document.getElementById("shapes10").appendChild(circle);
+  }, timer)
+
+  timer+=100;
+}
+
+for(var x = 0; x < 10; x++){
+
+  setTimeout(function(){
+
+    var hex = '0123456789ABCDEF';
+    var color = '#';
+
+    for (var z = 0; z < 6; z++) {
+      color += hex[Math.floor(Math.random() * 16)];
+    }
+
+    var circle = document.createElement("div");
+    circle.className="circle";
+    circle.style.backgroundColor = color;
+
+
+    document.getElementById("shapes11").appendChild(circle);
+  }, timer)
+
+  timer+=100;
+}
+
+for(var x = 0; x < 10; x++){
+
+  setTimeout(function(){
+
+    var hex = '0123456789ABCDEF';
+    var color = '#';
+
+    for (var z = 0; z < 6; z++) {
+      color += hex[Math.floor(Math.random() * 16)];
+    }
+
+    var circle = document.createElement("div");
+    circle.className="circle";
+    circle.style.backgroundColor = color;
+
+
+    document.getElementById("shapes12").appendChild(circle);
+  }, timer)
+
+  timer+=100;
+}
+
+for(var x = 0; x < 10; x++){
+
+  setTimeout(function(){
+
+    var hex = '0123456789ABCDEF';
+    var color = '#';
+
+    for (var z = 0; z < 6; z++) {
+      color += hex[Math.floor(Math.random() * 16)];
+    }
+
+    var circle = document.createElement("div");
+    circle.className="circle";
+    circle.style.backgroundColor = color;
+
+
+    document.getElementById("shapes13").appendChild(circle);
+  }, timer)
+
+  timer+=100;
+}
+
+for(var x = 0; x < 10; x++){
+
+  setTimeout(function(){
+
+    var hex = '0123456789ABCDEF';
+    var color = '#';
+
+    for (var z = 0; z < 6; z++) {
+      color += hex[Math.floor(Math.random() * 16)];
+    }
+
+    var circle = document.createElement("div");
+    circle.className="circle";
+    circle.style.backgroundColor = color;
+
+
+    document.getElementById("shapes14").appendChild(circle);
+  }, timer)
+
+  timer+=100;
+}
+
+for(var x = 0; x < 10; x++){
+
+  setTimeout(function(){
+
+    var hex = '0123456789ABCDEF';
+    var color = '#';
+
+    for (var z = 0; z < 6; z++) {
+      color += hex[Math.floor(Math.random() * 16)];
+    }
+
+    var circle = document.createElement("div");
+    circle.className="circle";
+    circle.style.backgroundColor = color;
+
+
+    document.getElementById("shapes15").appendChild(circle);
+  }, timer)
+
+  timer+=100;
+}
