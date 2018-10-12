57d8f1e1a636d8d6fd0dd3515e183fbfd28fb8a8
LeeJun Park <31444831+leejunp@users.noreply.github.com>
2018-10-12 05:17:16 -0400

    a

--- main4.js
+++ main4.js
@@ -1,27 +1,28 @@
 var timer = 0;
 var l=0;
 var w = 0;
-var circle = document.createElement("div");
-circle.className="circle";
-
-
-
 
 for(var x = 0; x < 500; x++){
 
-  function right(){
-    l = Math.random()*30+1;
-    w = Math.random()*30+1;
-    circle.style.marginLeft = l +"vw";
-    circle.style.marginTop = w +"vw";
-
-    // document.getElementById("shapes").appendChild(circle);
-  }
-
   setTimeout(function(){
+
+    var circle = document.createElement("div");
+    circle.className="circle";
 
     var hex = '0123456789ABCDEF';
     var color = '#';
+
+    // document.getElementById("myButton").addEventListener("click", right());
+    document.getElementById("myButton").onclick = function(){ right()};
+
+    function right(){
+      l = Math.random()*30+1;
+      w = Math.random()*30+1;
+      o = Math.random()*1;
+      circle.style.marginLeft = l +"vw";
+      circle.style.marginTop = w +"vw";
+      circle.style.opacity = o;
+    }
 
     for (var z = 0; z < 6; z++) {
       color += hex[Math.floor(Math.random() * 16)];
@@ -31,5 +32,5 @@ for(var x = 0; x < 500; x++){
     document.getElementById("shapes").appendChild(circle);
   }, timer)
 
-  timer+=100;
+timer+=100;
 }
